03262eec525760e607f2c4e2441ad46227c127b2
MoniqueHeusinger <m.heusinger1821@gmail.com>
2024-04-16 23:04:17 +0200

    add newsticker to TechStack.jsx

--- frontend/src/pageContent/techStack/TechStack.jsx
+++ frontend/src/pageContent/techStack/TechStack.jsx
@@ -8,11 +8,18 @@ const TechStack = () => {
     return (
         <Stack className="relative my-11">
             <h2>Tech Stack</h2>
-            <Container className="container text-center tech-stack-container mt-3">
-                <div className="row row-cols-auto flex-wrap gap-3 justify-content-between">
-                    {stackList.map((item, index) => (
-                        <span key={index} className="px-5 py-3 rounded-4  col">{item}</span>
-                    ))}
+            <Container className="container text-center tech-stack-container mt-3 ticker-wrap mx-auto overflow-hidden ">
+                <div className="row row-cols-auto flex-wrap gap-3 justify-content-between ticker d-inline-block">
+                    <span className="item-collection-1">
+                        {stackList.map((item, index) => (
+                            <span key={index} className="px-5 py-3 rounded-4  col item d-inline-block mx-3">{item}</span>
+                        ))}
+                    </span>
+                    <span className="item-collection-2">
+                        {stackList.map((item, index) => (
+                            <span key={index} className="px-5 py-3 rounded-4  col item d-inline-block mx-3">{item}</span>
+                        ))}
+                    </span>
 
                 </div>
             </Container>
